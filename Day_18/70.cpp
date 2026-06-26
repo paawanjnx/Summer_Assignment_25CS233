a190a64a850e0bd1adfd77f9c3b7d8e5a3d72606
paawanjnx <paawanjain6@gmail.com>
2026-06-26 18:25:06 +0530

    Day_18 uploaded.

--- Day_18/70.cpp
+++ Day_18/70.cpp
@@ -1,4 +1,4 @@
-// wap to bubble sort
+// wap to selection sort
 #include <iostream>
 using namespace std;
 
@@ -15,15 +15,19 @@ int main() {
         cin >> arr[i];
     }
 
-    // Bubble Sort
+    // Selection Sort
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        int min = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
             }
         }
+
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
     }
 
     cout << "Sorted array: ";
--- Day_18/72.cpp
+++ Day_18/72.cpp
@@ -1,4 +1,4 @@
-// wap to bubble sort
+// wap to sort array in descending order 
 #include <iostream>
 using namespace std;
 
@@ -15,18 +15,18 @@ int main() {
         cin >> arr[i];
     }
 
-    // Bubble Sort
+    // Sort in descending order
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 
-    cout << "Sorted array: ";
+    cout << "Array in descending order: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
